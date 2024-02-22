d49d17c79e924c6c71435b788fb70e2521f6a9f9
student <student@ex>
2024-02-22 15:14:15 +0700

    Internet-shop

--- www/resources/views/auth/registration.blade.php
+++ www/resources/views/auth/registration.blade.php
@@ -3,23 +3,24 @@
 @section('content')
 
 <section class="registration">
-    <p>Registration</p>
-    <div class="container-form">
-        <form class="form-register" action="{{ route('auth.registration') }}" method="post">
-            @csrf
-            <input type="text" name="name" placeholder="name">
-            <input type="text" name="surname" placeholder="surname">
-            <input type="text" name="patronymic" placeholder="patronymic">
-            <input type="text" name="login" placeholder="login">
-            <input type="email" name="email" placeholder="email">
-            <input type="password" name="password" placeholder="password">
-            <input type="password" name="password_repeat" placeholder="password_repeat">
-            <label for="rules">Sure</label>
-            <input type="checkbox" id="rules" name="rules" required>
-            <input type="submit">
-        </form>
+    <div class="background">
+        <p>Registration</p>
+        <div class="container-form">
+            <form class="form-register" action="{{ route('auth.registration') }}" method="post">
+                @csrf
+                <input class="input" type="text" name="name" placeholder="name">
+                <input class="input" type="text" name="surname" placeholder="surname">
+                <input class="input" type="text" name="patronymic" placeholder="patronymic">
+                <input class="input" type="text" name="login" placeholder="login">
+                <input class="input" type="email" name="email" placeholder="email">
+                <input class="input" type="password" name="password" placeholder="password">
+                <input class="input" type="password" name="password_repeat" placeholder="password_repeat">
+                <label class="p-checkbox" for="rules">Sure</label>
+                <input type="checkbox" id="rules" name="rules" required>
+                <input class="button" value="Sign up" type="submit">
+            </form>
+        </div>
     </div>
-
 </section>
 
 

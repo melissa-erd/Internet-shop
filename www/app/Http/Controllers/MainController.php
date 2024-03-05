12e74ce2820815dad68fab786d29fbbb30d8eaf8
student <student@ex>
2024-03-05 15:02:44 +0700

    Internet-shop 05.03.24

--- www/app/Http/Controllers/MainController.php
+++ www/app/Http/Controllers/MainController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use App\Models\Production;
+use App\Models\User;
+use App\Services\BasketService;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -14,17 +17,40 @@ class MainController extends Controller
         return view('index', compact('productions'));
     }
 
-    public function productions()
+    public function productions(Request $request, BasketService $basketService)
     {
-        $productions = Production::query()->orderByDesc('id')->get();
+        $productions = Production::all();
+
+        if ($user = auth()->user()) {
+            $productions = Production::query()->with('baskets')->with([
+                'baskets' => function (Builder $builder) use ($user, $basketService) {
+                    $builder->where('baskets.id', $basketService->resolve($user)->id);
+                }
+            ]);
+        }
+
+        $productions = $productions->orderByDesc('id')->get();
         $genres = Genre::all();
 
+        if ($request->date === "old") {
+            $productions = Production::query()->orderBy('date')->get();
+        }
 
-//        if (isset($request->new))
-//        {
-//            $productions = Production::query()->orderByDesc('date')->get();
+        if ($request->date === 'new') {
+            $productions = Production::query()->orderByDesc('date')->get();
+        }
+
+        if ($request->name === 'alphabet') {
+            $productions = Production::query()->orderBy('name')->get();
+        }
+
+        if ($request->age === 'ageToHigh') {
+            $productions = Production::query()->orderBy('age')->get();
+        }
+//        if (isset($request->genres)) {
+//            $genres->whereRelation('genres', 'id', $request->genres);
 //        }
-
         return view('productions', compact('productions', 'genres'));
     }
+
 }
--- www/resources/views/productions.blade.php
+++ www/resources/views/productions.blade.php
@@ -1,23 +1,73 @@
 @extends('app')
 @section('content')
 
-    <section class="afisha">
-        <button name="new">New</button>
+    <section>
+        <div class="sort-filter">
+            <div class="dropdown">
+                <button class="button dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="button-a">Sort</a>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{route("productions")}}?date=old">Snachala blizhayshie</a></li>
+                    <li><a class="dropdown-item" href="{{route("productions")}}?date=new">Snachala pozdnie</a></li>
+                    <li><a class="dropdown-item" href="{{ route('productions') }}?name=alphabet">A-Z</a></li>
+                    <li><a class="dropdown-item" href="{{ route('productions') }}?age=ageToHigh">Age</a></li>
+                </ul>
+            </div>
+            <div>
+                <form class="filter" action="">
+                    <select class="form-select" aria-label="Default select example" name="genres[]">
+                        <option hidden="" value="">Select genre</option>
+                        @foreach($genres as $genre)
+                            <option value="{{ $genre->id }}">{{ $genre->name }}</option>
+                        @endforeach
+                    </select>
+                    <button class="button button-a">Search</button>
+                </form>
+            </div>
+        </div>
+
+
     <div class="afisha-items">
     @foreach($productions as $production)
         <div class="card" style="width: 18rem;">
             <img src="{{ Storage::URL($production->picture) }}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ $production->name }}</h5>
-                <p class="card-text">{{ $production->genres()->implode('name',', ') }}</p>
-                <p class="card-text">{{ $production->date }}</p>
-                <p class="card-text">{{ $production->age }}</p>
-                <p class="card-text">{{ $production->price }}</p>
+                <p class="card-text">Show date: {{ $production->date }}</p>
+                <p class="card-text">Genre: {{ $production->genres()->implode('name',', ') }}</p>
+                <p class="card-text">Price: {{ $production->price }}₽</p>
+                <p class="card-text">{{ $production->age }} +</p>
                 @guest()
-                    <p>If you want to buy a ticket you should to authorize</p>
+                    <div class="card">
+                        <div class="card-body">
+                            If you want to buy a ticket you should to authorize
+                        </div>
+                    </div>
+{{--                    <p>If you want to buy a ticket you should to authorize</p>--}}
                 @endguest
+
                 @auth()
-                    <button class="button"><a href="#" class="button-a">Buy a ticket</a></button>
+                    <div class="add_subtract">
+                        <form action="{{ route('basket.add') }}" method="post">
+                            @csrf
+                            <input type="hidden" name="production_id" value="{{ $production->id }}">
+                            <input class="button btn-add-subtract" type="submit" value="+">
+                        </form>
+                            @if($production->baskets->first())
+                                <p class="count">{{ $production->baskets->first()->pivot->count }}</p>
+                            @endif
+                        @if(!is_null($production->baskets->first()))
+                            <form action="{{ route('basket.subtract') }}" method="post">
+                                @csrf
+                                <input type="hidden" name="production_id" value="{{ $production->id }}">
+                                <input class="button btn-add-subtract" type="submit" value="-">
+                            </form>
+                        @endif
+                    </div>
+
+
+{{--                    <button class="button"><a href="#" class="button-a">Buy a ticket</a></button>--}}
                 @endauth
             </div>
         </div>
